c45b8deb8beb2bd99037e891fd40632f3c9dd8dc
zendeaashay <148306495+zendeaashay@users.noreply.github.com>
2024-06-27 18:46:45 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -150,13 +150,13 @@ def main():
 
                  You can add SQL statements and functions to a view and present the data as if the data were coming from one single table.
 
-        A view is created with the CREATE VIEW statement. 
-                     CREATE VIEW Syntax
-                     CREATE VIEW view_name AS
-                     SELECT column1, column2, ...
-                     FROM table_name
-                     WHERE condition;
-        Note: A view always shows up-to-date data! The database engine recreates the view, every time a user queries it.""")
+            A view is created with the CREATE VIEW statement. 
+                 CREATE VIEW Syntax
+                 CREATE VIEW view_name AS
+                 SELECT column1, column2, ...
+                 FROM table_name
+                 WHERE condition;
+                 Note: A view always shows up-to-date data! The database engine recreates the view, every time a user queries it.""")
         st.write(result)
 
     # Search operations
