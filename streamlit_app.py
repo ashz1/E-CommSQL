4393919342517c70f746f90059ee6baaf272fe5f
zendeaashay <148306495+zendeaashay@users.noreply.github.com>
2024-06-27 18:42:16 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -144,7 +144,8 @@ def main():
             SQL CREATE VIEW Statement
              In SQL, a view is a virtual table based on the result-set of an SQL statement.
 
-             A view contains rows and columns, just like a real table. The fields in a view are fields from one or more real tables in the database.
+             
+                 A view contains rows and columns, just like a real table. The fields in a view are fields from one or more real tables in the database.
 
              You can add SQL statements and functions to a view and present the data as if the data were coming from one single table.
 
